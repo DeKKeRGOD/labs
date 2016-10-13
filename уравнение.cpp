ee2b574a1dd15b7813b62c8c31acf32110839684
DeKKeRGOD <tigrenvova@mail.ru>
2016-10-13 19:50:26 +0400

    Add files via upload

---
+++ уравнение.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <math.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	double y; // Определение переменных
+	cout << "x="; // Вывод фразы "x=" на экран
+	cin >> x; // Ввод данных пользователем
+	y = x+sqrt(6-x)-6; // Уравнение
+	cout << "y="; // Вывод фразы "y=" на экран
+	cout << y; // Вывод получившейся переменной на экран
+	getchar ();
+
+	return 0;
+}
+
